63b741b582b83c6e1eb58af8bb824dec575dfe50
DzKInShO2 <dzkinsho2@protonmail.com>
2024-06-01 11:53:59 +0800

    tambahkan cara menampilkan laman bantuan

--- yw.cpp
+++ yw.cpp
@@ -12,13 +12,20 @@ void help_page()
          << "e encrypt text\n"
          << "d decrypt text\n"
          << "Options:\n"
-         << " -h show this page after you enter mode\n";
+         << " -h show this page\n";
 }
 
 int main(int argc, const char* argv[])
 {
     string text = "";
     if (argc > 2) {
+        // Tampilkan laman bantuan
+        // dan selesai
+        if (string(argv[2]) == "-h") {
+            help_page();
+            return 0;
+        }
+
         // Iterasi daftar [options]
         for (int i = 2; i < argc - 1; ++i) {
             switch (argv[i][1]) {

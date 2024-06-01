0c8f4e4f8346a09778fc39251f87fa2cbefe6530
DzKInShO2 <dzkinsho2@protonmail.com>
2024-06-01 11:31:06 +0800

    Tambah help page

--- yw.cpp
+++ yw.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
 using namespace std;
 
+/*
+ * Menampilkan laman bantuan
+ */
+void help_page()
+{
+    cout << "Usage: yw [mode] [options]... text\n"
+         << "Encrypt or decrypt text based on the options given.\n";
+}
+
 int main(int argc, const char* argv[])
 {
+    if (argc > 2) {
+
+    } else help_page();
+
     return 0;
 }

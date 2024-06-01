5054f27e6c941becdb05c4c0f5b796b94399dbae
DzKInShO2 <dzkinsho2@protonmail.com>
2024-06-01 11:46:53 +0800

    Tambah laman bantuan

--- yw.cpp
+++ yw.cpp
@@ -7,13 +7,26 @@ using namespace std;
 void help_page()
 {
     cout << "Usage: yw [mode] [options]... text\n"
-         << "Encrypt or decrypt text based on the options given.\n";
+         << "Encrypt or decrypt text based on the options given.\n\n"
+         << "Mode:\n"
+         << "e encrypt text\n"
+         << "d decrypt text\n"
+         << "Options:\n"
+         << " -h show this page after you enter mode\n";
 }
 
 int main(int argc, const char* argv[])
 {
+    string text = "";
     if (argc > 2) {
-
+        // Iterasi daftar [options]
+        for (int i = 2; i < argc - 1; ++i) {
+            switch (argv[i][1]) {
+                // Laman bantuan
+                case 'h': help_page();
+                break;
+            }
+        }
     } else help_page();
 
     return 0;
